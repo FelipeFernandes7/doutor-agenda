8dd5ae571022d6b2eed17d45044d2f66318a0a38
Felipe Fernandes <felipekurosaki123@gmail.com>
2026-06-17 18:54:19 -0300

    feat: implement clinic creation logic with session validation

--- src/actions/create-clinic/index.ts
+++ src/actions/create-clinic/index.ts
@@ -11,9 +11,11 @@ export const createClinic = async (name: string) => {
   const session = await auth.api.getSession({
     headers: await headers(),
   });
+
   if (!session?.user) {
     throw new Error("Unauthorized");
   }
+
   const [clinic] = await db.insert(clinicsTable).values({ name }).returning();
   await db.insert(usersToClinicsTable).values({
     userId: session.user.id,
