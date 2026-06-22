c64d3b1aec64aad4c0e55e7a6f2c8d8e57b80851
Felipe Fernandes <felipekurosaki123@gmail.com>
2026-06-22 16:24:07 -0300

    refactor: improve session handling and clinic validation in dashboard and doctors pages

--- src/app/(protected)/dashboard/page.tsx
+++ src/app/(protected)/dashboard/page.tsx
@@ -1,9 +1,6 @@
-import { eq } from "drizzle-orm";
 import { headers } from "next/headers";
 import { redirect } from "next/navigation";
 
-import { db } from "@/db";
-import { usersToClinicsTable } from "@/db/schema";
 import { auth } from "@/lib/auth";
 
 import { SignOutButton } from "./_components/sign-out-button";
@@ -17,11 +14,7 @@ export default async function DashboardPage() {
     redirect("/authentication");
   }
 
-  const clinics = await db.query.usersToClinicsTable.findMany({
-    where: eq(usersToClinicsTable.userId, session.user.id),
-  });
-
-  if (clinics.length === 0) {
+  if (!session.user.clinic) {
     redirect("/clinic-form");
   }
 
@@ -30,16 +23,6 @@ export default async function DashboardPage() {
       <div className="flex flex-col space-y-4">
         <h1>{session?.user.name}</h1>
         <h1>{session?.user.email}</h1>
-        {clinics.length > 0 &&
-          clinics.map((clinic, index) => (
-            <div
-              key={index}
-              className="flex w-full flex-col space-y-2 rounded-2xl border border-gray-300 bg-gray-100 p-3"
-            >
-              <h1>{clinic.clinicId}</h1>
-              <p>{new Date(clinic.createdAt).toLocaleDateString("pt-BR")}</p>
-            </div>
-          ))}
       </div>
       <SignOutButton />
     </div>
