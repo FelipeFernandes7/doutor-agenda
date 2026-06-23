f15447de4b3043947c041b950956923e58ac324b
Felipe Fernandes <felipekurosaki123@gmail.com>
2026-06-23 15:23:34 -0300

    feat: implement doctor management features with availability display and currency formatting

--- src/app/(protected)/doctors/page.tsx
+++ src/app/(protected)/doctors/page.tsx
@@ -1,3 +1,4 @@
+import { eq } from "drizzle-orm";
 import { headers } from "next/headers";
 import { redirect } from "next/navigation";
 
@@ -10,9 +11,12 @@ import {
   PageHeaderContent,
   PageTitle,
 } from "@/components/ui/page-container";
+import { db } from "@/db";
+import { doctorsTable } from "@/db/schema";
 import { auth } from "@/lib/auth";
 
 import { AddDoctorButton } from "./_components/add-doctor-button";
+import { DoctorCard } from "./_components/doctor-card";
 
 export default async function DoctorsPage() {
   const session = await auth.api.getSession({ headers: await headers() });
@@ -24,20 +28,29 @@ export default async function DoctorsPage() {
   if (!session.user.clinic) {
     redirect("/clinic-form");
   }
+  const doctors = await db.query.doctorsTable.findMany({
+    where: eq(doctorsTable.clinicId, session.user.clinic.id),
+  });
 
   return (
     <PageContainer>
       <PageHeader>
         <PageHeaderContent>
           <PageTitle>Médicos</PageTitle>
-          <PageDescription>Gerencie os médicos e sua clínica </PageDescription>
+          <PageDescription>
+            Access a detailed overview of key metrics and patient outcomes
+          </PageDescription>
         </PageHeaderContent>
         <PageActions>
           <AddDoctorButton />
         </PageActions>
       </PageHeader>
       <PageContent>
-        <h1>Médicos</h1>
+        <div className="grid grid-cols-3 gap-6">
+          {doctors.map((doctor) => (
+            <DoctorCard key={doctor.id} doctor={doctor} />
+          ))}
+        </div>
       </PageContent>
     </PageContainer>
   );
